333db3c71008a3de01f72f81dd978b3b7a621d2c
ec16544 <i.s.shalev@se16.qmul.ac.uk>
2017-03-21 14:15:04 +0000

    fixed the folder and added some code

--- setup.py
+++ setup.py
@@ -7,5 +7,5 @@ setup(name='FBA',
       author_email='ivoshalev@gmail.com',
       url='https://github.com/Iviglious',
       #install_requires=['Flask>=0.7.2', 'MarkupSafe'],
-      install_requires=['Flask>=0.7.2','flask-wtf','flask-babel','markdown','flup']
+      install_requires=['Flask>=0.7.2','flask-wtf','flask-babel','markdown','flup', 'unirest']
       )
