378cf2e1de4ccb6d9e19f7e4b2224a9a89aedb5e
ec16544 <i.s.shalev@se16.qmul.ac.uk>
2017-03-21 13:16:54 +0000

    Added first draft of the custom app

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup
 
-setup(name='YourAppName',
+setup(name='FBA',
       version='1.0',
       description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
-      url='https://www.python.org/community/sigs/current/distutils-sig',
-      install_requires=['Flask>=0.7.2', 'MarkupSafe'],
+      author='Ivaylo Shalev',
+      author_email='ivoshalev@gmail.com',
+      url='https://github.com/Iviglious',
+      #install_requires=['Flask>=0.7.2', 'MarkupSafe'],
+      install_requires=['Flask>=0.7.2','flask-wtf','flask-babel','markdown','flup']
       )
