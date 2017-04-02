448e47ff4b092329999a4325b2ca4f39a111b4a0
ec16544 <i.s.shalev@se16.qmul.ac.uk>
2017-04-02 21:46:52 +0100

    Changed to print just the URI

--- wsgi/app/demo_db.py
+++ wsgi/app/demo_db.py
@@ -10,10 +10,11 @@ from app import C_APP
 C_APP.config['MONGO_DBNAME'] = os.environ['OPENSHIFT_APP_NAME']
 C_APP.config['MONGO_URI'] = os.environ['OPENSHIFT_MONGODB_DB_URL']
 
-MG_DB = PyMongo(C_APP)
+#MG_DB = PyMongo(C_APP)
 
 @C_APP.route('/demo_db')
 def demo_db():
     """ Function to list the DB's """
 
-    return jsonify({'result': MG_DB.db.collection_names()})
+    return jsonify({'result': C_APP.config['MONGO_URI']})
+    #return jsonify({'result': MG_DB.db.collection_names()})
