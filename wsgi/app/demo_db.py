f97179872355e76041efedc25700b1a62847aaa2
ec16544 <i.s.shalev@se16.qmul.ac.uk>
2017-04-03 01:08:55 +0100

    Connected DB functions with web page

--- wsgi/app/demo_db.py
+++ wsgi/app/demo_db.py
@@ -46,15 +46,25 @@ def show_all_expenses():
     html_str += "<th>expense_date</th>"
     html_str += "<th>expense_amount</th>"
     html_str += "<th>expense_description</th>"
+    html_str += "<th>id</th>"
     html_str += "</tr>"
 
     for exp in MG_DB.db.expenses.find():
         html_str += "<tr>"
         html_str += "<td>{}</td>".format(exp['user_name'])
-        html_str += "<td>{:%Y-%m-%d %H:%M}</td>".format(exp['expense_date'])
-        html_str += "<td>{}</td>".format(exp['expense_amount'])
+        html_str += "<td>{:%Y-%m-%d %H:%M} {}</td>".format(exp['expense_date'], type(exp['expense_date']))
+        html_str += "<td>{} {}</td>".format(exp['expense_amount'], type(exp['expense_amount']))
         html_str += "<td>{}</td>".format(exp['expense_description'])
+        html_str += "<td>{}</td>".format(exp['id'])
         html_str += "</tr>"
     html_str += "</table>"
     return html_str
 
+
+@C_APP.route('demo_db/remove/<row_id>')
+def remove_row(row_id):
+    """ Function for removal of row from expenses collection """
+
+    res = MG_DB.db.expenses.delete_one(row_id)
+    return "Number of rows removed: {}<br><br>\
+    View all <a href='/demo_db/expenses'>expenses</a>".format(res.deleted_count)
